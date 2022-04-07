6a5f72805999a4db0b153d3d7c54dbc048c4391b
DidoFayed <fayeddido.19051@mhs.its.ac.id>
2022-04-07 13:29:30 +0700

    commit

---
+++ invita_frameprog/database/migrations/2021_12_10_081040_create_undangans_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUndangansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('undangans', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('id_jenis_undangan')->references('id')->on('jenis_undangans');
+            $table->foreignId('id_order')->references('id')->on('orders');
+            $table->string('tema_undangan');
+            $table->date('tanggal_mulai_acara');
+            $table->date('tanggal_berakhir_acara');
+            $table->time('waktu_mulai_acara', $precision = 0);
+            $table->time('waktu_berakhir_acara', $precision = 0);
+            $table->string('lokasi_acara');
+            $table->string('alamat_acara');
+            $table->string('slug')->nullable();
+            $table->boolean('peta_google')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('undangans');
+    }
+}
---
+++ invita_frameprog/database/migrations/2021_12_10_081458_create_undangan_ulang_tahuns_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUndanganUlangTahunsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('undangan_ulang_tahuns', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('id_undangan')->references('id')->on('undangans');
+            $table->char('honorific_ulangtahun', 5);
+            $table->string('nama_ulangtahun');
+            $table->date('tanggal_lahir_ulangtahun');
+            $table->text('deskripsi_ulangtahun')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('undangan_ulang_tahuns');
+    }
+}
